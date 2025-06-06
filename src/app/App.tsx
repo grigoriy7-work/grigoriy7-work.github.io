ca027b31a752ad0845c3fdd330fff3e90284f034
Grigoriy Abashkin <ab.grigoriy10@gmail.com>
2025-06-06 21:07:43 +0500

    feat: add info about me

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,7 +8,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          <b>Цель: </b>
+          <span>расширить свой кругозор современных технологий и научиться создавать готовые фронтенд проекты</span>
+          <br />
+          <b>Владею технологиями: </b>
+          <span>c#, net framework, dot net, mssql, rabbitmq. JavaScript, jquery, node js</span>
+          <br />
+          <b>О себе: </b>
+          <span>
+            Абашкин Григорий. Мне 27 лет. Работаю в компаии Kazzinc.
+            <br />
+            Разрабатываю c# приложение для предприятия, около 6 лет. Работаю с net framework и dot net.
+          </span>
         </p>
       </header>
     </div>
