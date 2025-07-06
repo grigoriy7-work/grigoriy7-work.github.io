add534a9e1ecb048f1f7600e215c35807b42f794
Grigoriy Abashkin <ab.grigoriy10@gmail.com>
2025-07-06 16:34:07 +0500

    feat: restore app.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-//import { ModalWindow } from './../homeworks/components/ModalWindow';
-import { Header } from './../homeworks/components/Header';
 
 function App() {
   return (
-    <div>
-      <Header />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Текст писать тут</p>
+      </header>
     </div>
   );
 }
